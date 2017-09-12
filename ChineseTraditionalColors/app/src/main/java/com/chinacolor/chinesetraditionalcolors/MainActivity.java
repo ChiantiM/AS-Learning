0714bb896a6e619d6c45a454e6f581dd664b13e0
Chealin <lingnanmiao@gmail.com>
2017-09-12 17:27:56 +0800

    增加了部分颜色

--- ChineseTraditionalColors/app/src/main/java/com/chinacolor/chinesetraditionalcolors/MainActivity.java
+++ ChineseTraditionalColors/app/src/main/java/com/chinacolor/chinesetraditionalcolors/MainActivity.java
@@ -17,6 +17,10 @@ import java.util.List;
 
 /**
  * 已实现：变换背景色功能
+ *
+ * Todo:
+ * 实现Text淡入淡出
+ * rgb解析
  */
 public class MainActivity extends AppCompatActivity {
     private View bg;
@@ -25,11 +29,15 @@ public class MainActivity extends AppCompatActivity {
     private Typeface tf;
 
 
-    public int currentColor = 0xff973444;
+    public int currentColorpos = 0;
     public static final int[] colorValue = {0xff973444, 0xffcc3536, 0xffc43739,0xffdd3b44,0xffdc143c,
-    0xffeacdd1, 0xffbb1c33, 0xff89303f, 0xffa54358, 0xff674950};
+    0xffeacdd1, 0xffbb1c33, 0xff89303f, 0xffa54358, 0xff674950,
+    0xff643441, 0xff79485a, 0xff793d56, 0xff9c6680, 0xff3e3c3d,
+    0xffa71368, 0xffeea5d1, 0xffa22076, 0xffc3a6cb, 0xffab96c5};
     public String[] colorname = {"玫瑰红", "艳红", "猩红", "银朱","洋红"
-    ,"浅血牙","月季红", "枣红", "紫粉","茄皮紫"};
+    ,"浅血牙","月季红", "枣红", "紫粉","茄皮紫",
+    "深烟红", "雪紫", "玫瑰灰", "洋葱紫", "元青",
+    "品红", "紫薇花", "牵牛紫", "紫水晶", "浅石英紫"};
     public List<Color> colorList;
 
 
@@ -45,12 +53,12 @@ public class MainActivity extends AppCompatActivity {
         colorList = new ArrayList<Color>();
         colorList_init();
         title = (TextView)findViewById(R.id.color_title);
-        bg.setBackgroundColor(currentColor);
+        bg.setBackgroundColor(colorValue[currentColorpos]);
 
         //设置typeface
         tf = Typeface.createFromAsset(getAssets(),"fonts/tieshankaishujian.ttf");
         title.setTypeface(tf);
-        title.setText("玫瑰红");
+        title.setText(colorname[currentColorpos]);
 
         //设置适配器
         ColorAdapter adapter = new ColorAdapter(MainActivity.this, R.layout.color_item, colorList);
@@ -62,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 ((TextView)findViewById(R.id.color_title)).setText(colorname[i]);
 
-                bg_crossFade(currentColor, colorValue[i]);
-                currentColor = colorValue[i];
+                bg_crossFade(colorValue[currentColorpos], colorValue[i]);
+                currentColorpos = i;
                 Log.d("MainActivity", "点击gview项目成功");
             }
         });
@@ -93,6 +101,14 @@ public class MainActivity extends AppCompatActivity {
             color = new Color(colorname[i], colorValue[i]);
             colorList.add(color);
         }
+    }
+
+    public int[] parseRGB(int colorValue){
+        int[] rgb = {0,0,0};
+        rgb[0] = colorValue>>>16;
+        rgb[1] = (colorValue<<8)>>>16;
+        rgb[3] = (colorValue<<16)>>>16;
+        return rgb;
     }
 
 }
