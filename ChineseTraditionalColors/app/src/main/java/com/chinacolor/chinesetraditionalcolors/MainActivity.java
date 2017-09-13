d9ab364e834dfa525048cbd63e27bdb550c89f8a
Chealin <lingnanmiao@gmail.com>
2017-09-13 22:36:22 +0800

    增加颜色，数据库，收藏功能

--- ChineseTraditionalColors/app/src/main/java/com/chinacolor/chinesetraditionalcolors/MainActivity.java
+++ ChineseTraditionalColors/app/src/main/java/com/chinacolor/chinesetraditionalcolors/MainActivity.java
@@ -4,7 +4,11 @@ import android.animation.AnimatorSet;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,18 +16,25 @@ import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 已实现：变换背景色功能
+ * 已实现：
+ * 变换背景色功能
+ * Text淡入淡出
+ * rgb解析
+ *
+ * 注意：
+ * 在插入，删除，移动时，保持gview与colorlist的顺序一致
+ * 保持colorValue和colorname数目一致
  *
  * Todo:
- * 实现Text淡入淡出
- * rgb解析
+ * 添加actionbar, 展示收藏项目
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private View bg;
     private GridView gView;
     private TextView title;
@@ -31,34 +42,48 @@ public class MainActivity extends AppCompatActivity {
     private TextView color_r;
     private TextView color_g;
     private TextView color_b;
-
-
+    private ImageView favor;
 
     public int currentColorpos = 0;
     public static final int[] colorValue = {0xff973444, 0xffcc3536, 0xffc43739,0xffdd3b44,0xffdc143c,
     0xffeacdd1, 0xffbb1c33, 0xff89303f, 0xffa54358, 0xff674950,
     0xff643441, 0xff79485a, 0xff793d56, 0xff9c6680, 0xff3e3c3d,
-    0xffa71368, 0xffeea5d1, 0xffa22076, 0xffc3a6cb, 0xffab96c5};
+    0xffa71368, 0xffeea5d1, 0xffa22076, 0xffc3a6cb, 0xffab96c5,
+    0xff4e1892, 0xff857e95, 0xffc4c3cb, 0xff1f3696, 0xff1b54f2,
+    0xff25386b, 0xff43454a, 0xff0041a5, 0xff3c5e91, 0xff546b83,
+    0xff455667, 0xff17507d, 0xff2578b5, 0xff31678d, 0xff276893,
+    0xff2b5e7d, 0xff6493af, 0xff37444b, 0xff4f5355, 0xff93a2a9,
+    0xff507883, 0xff2ec3e7, 0xff5d828a, 0xff7ba1a8, 0xff748a8d};
     public String[] colorname = {"玫瑰红", "艳红", "猩红", "银朱","洋红"
     ,"浅血牙","月季红", "枣红", "紫粉","茄皮紫",
     "深烟红", "雪紫", "玫瑰灰", "洋葱紫", "元青",
-    "品红", "紫薇花", "牵牛紫", "紫水晶", "浅石英紫"};
+    "品红", "紫薇花", "牵牛紫", "紫水晶", "浅石英紫",
+    "柏坊灰蓝", "紫藤灰", "浅藤紫", "宝蓝", "靛蓝",
+    "藏蓝", "粗晶皂", "孔雀蓝", "浅海昌蓝", "花青",
+    "鹊灰", "海蓝", "深竹月", "绒蓝", "北京毛蓝",
+    "沙青", "钻蓝", "铁灰", "红皂", "正灰",
+    "玉石蓝", "天青", "灰蓝", "春蓝", "织锦灰"};
     public List<Color> colorList;
 
+    public final String color_dir = "content://com.chinacolor.chinesetraditionalcolors.provider/color";
+    public Uri uri = Uri.parse(color_dir);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         //initialize
         bg = findViewById(R.id.background);
         gView = (GridView)findViewById(R.id.gview);
+        favor = (ImageView)findViewById(R.id.favor);
         colorList = new ArrayList<Color>();
         colorList_init();
         title = (TextView)findViewById(R.id.color_title);
         title.setText(colorname[currentColorpos]);
+
+        //设置rgb文字
         color_r = (TextView)findViewById(R.id.color_R);
         color_g = (TextView) findViewById(R.id.color_G);
         color_b = (TextView) findViewById(R.id.color_B);
@@ -72,6 +97,16 @@ public class MainActivity extends AppCompatActivity {
         tf = Typeface.createFromAsset(getAssets(),"fonts/hanyiquantangshijian.ttf");
         title.setTypeface(tf);
 
+        //初始化收藏图标
+        Cursor cursor = getContentResolver().query(uri, new String[]{"favorite"}, "value = ?", new String[]{Integer.toHexString(colorValue[currentColorpos])}, null);
+        if (cursor.moveToFirst()) {
+            if (cursor.getInt(cursor.getColumnIndex("favorite")) == 1) {
+                favor.setBackgroundResource(R.drawable.icon_1);
+                Log.d("MainAcitivity", "检测到收藏");
+            }
+        }
+
+
         //设置gView适配器
         ColorAdapter adapter = new ColorAdapter(MainActivity.this, R.layout.color_item, colorList);
         gView.setAdapter(adapter);
@@ -80,8 +115,67 @@ public class MainActivity extends AppCompatActivity {
         gView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                crossfade(colorValue[currentColorpos], colorValue[i],colorname[i]);
+                crossfade(colorValue[currentColorpos], colorValue[i], colorname[i]);
+                //isFavor
+                Cursor cursor = getContentResolver().query(uri, new String[]{"favorite"}, "value = ?", new String[]{Integer.toHexString(colorValue[i])}, null);
+                if (cursor != null) {
+                    if (cursor.moveToFirst()) {
+                        if (cursor.getInt(cursor.getColumnIndex("favorite")) == 1) {
+                            favor.setBackgroundResource(R.drawable.icon_1);
+                            Log.d("MainAcitivity", "检测到收藏");
+                        }else {
+                            favor.setBackgroundResource(R.drawable.icon_0);
+                            Log.d("MainAcitivity", "检测到未收藏");
+                        }
+                    } else {
+                        favor.setBackgroundResource(R.drawable.icon_0);
+                        Log.d("MainAcitivity", "查询成功，无数据（无法移动到First）");
+                    }
+                }else{Log.d("MainAcitivity", "Cursor为空，查询失败");}
+
                 currentColorpos = i;
+            }
+        });
+
+        // favoraite button监听
+        favor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = getContentResolver().query(uri, null, "value = ?", new String[]{Integer.toHexString(colorValue[currentColorpos])}, null);
+                if (cursor.moveToFirst()){
+                    //if (cursor.moveToFirst()) {
+                        int isFavor = cursor.getInt(cursor.getColumnIndex("favorite"));
+                        String value = cursor.getString(cursor.getColumnIndex("value"));
+                        if (isFavor == 1){
+                            favor.setBackgroundResource(R.drawable.icon_0);
+                            //update新的数据
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put("favorite", 0);
+                            int rows = getContentResolver().update(uri, contentValues, "value = ?", new String[]{value});
+                            Log.d("MainActivity", "取消收藏成功: \n rows = " + rows +"\n value = " + value);
+
+                        }else{
+                            favor.setBackgroundResource(R.drawable.icon_1);
+                            //update
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put("favorite", 1);
+                            int rows = getContentResolver().update(uri, contentValues, "value = ?", new String[]{value});
+                            Log.d("MainActivity", "添加收藏成功: \n rows = " + rows +"\n value = " + value);
+                        }
+                    //}
+                }
+                else {
+                    favor.setBackgroundResource(R.drawable.icon_1);
+                    //insert
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put("value", Integer.toHexString(colorValue[currentColorpos]));
+                    contentValues.put("name", colorname[currentColorpos]);
+                    contentValues.put("favorite", 1);
+                    Uri newuri = getContentResolver().insert(uri, contentValues);
+                    Log.d("MainActivity", "新增到收藏: \n newuri = " + newuri
+                            + "\n value = " + Integer.toHexString(colorValue[currentColorpos])
+                            + "\n name = " + colorname[currentColorpos]);
+                }
 
             }
         });
